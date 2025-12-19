e14128f83c0f0d68a71a392be0a5e8e6ad76b541
yulin0308 <brian940308@gmail.com>
2025-12-19 18:36:32 +0800

    Add files via upload

---
+++ views.js
@@ -0,0 +1,15 @@
+import { ArraysView } from './views/arrays.js';
+import { StacksQueuesView } from './views/stacks_queues.js';
+import { LinkedListsView } from './views/linked_lists.js';
+import { TreesView } from './views/trees.js';
+import { GraphsView } from './views/graphs.js';
+import { BigOView } from './views/bigo.js';
+
+export const Views = {
+  arrays: ArraysView,
+  stacks: StacksQueuesView,
+  linkedlists: LinkedListsView,
+  trees: TreesView,
+  graphs: GraphsView,
+  bigO: BigOView
+};
